e8ae9a15b8cf1e60a4c5cd3b865b4b0dc3df8a2c
baowei.du <baowei.du@horizon.ai>
2019-04-07 00:37:05 +0800

    build: add clean func

--- build.sh
+++ build.sh
@@ -1,23 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-config=$UBOOT_DEFCONFIG
-echo "uboot config: $config"
+function all()
+{
+    prefix=$TARGET_UBOOT_DIR
+    config=$UBOOT_DEFCONFIG
+    echo "uboot config: $config"
 
-cd $(dirname "$0";pwd)
-ARCH=$ARCH_UBOOT
-make $config || {
-    echo "make $config failed"
-    exit 1
+    # real build
+    ARCH=$ARCH_KERNEL
+    make $config || {
+        echo "make $config failed"
+        exit 1
+    }
+    make -j${N} || {
+        echo "make failed"
+        exit 1
+    }
+
+    # put binaries to dest directory
+    cpfiles "$UBOOT_IMAGE_NAME" "$prefix/"
+    cd spl/
+    cpfiles "$UBOOT_SPL_NAME" "$prefix/"
 }
-make -j${N}
 
-if [ -f $SRC_UBOOT_DIR/$SRC_UBOOT_IMAGE_NAME -a -n $TARGET_UBOOT_DIR/$TARGET_UBOOT_IMAGE_NAME ];then
-    mkdir -p $TARGET_UBOOT_DIR
-    echo "cp -f $SRC_UBOOT_DIR/$SRC_UBOOT_IMAGE_NAME $TARGET_UBOOT_DIR/$TARGET_UBOOT_IMAGE_NAME"
-    cp -f $SRC_UBOOT_DIR/$SRC_UBOOT_IMAGE_NAME $TARGET_UBOOT_DIR/$TARGET_UBOOT_IMAGE_NAME
-fi
-if [ -f $SRC_UBOOT_DIR/spl/$SRC_UBOOT_SPL_NAME -a -n $TARGET_UBOOT_DIR/$TARGET_UBOOT_SPL_NAME ];then
-    mkdir -p $TARGET_UBOOT_DIR
-    echo "cp -f $SRC_UBOOT_DIR/spl/$SRC_UBOOT_SPL_NAME $TARGET_UBOOT_DIR/$TARGET_UBOOT_SPL_NAME"
-    cp -f $SRC_UBOOT_DIR/spl/$SRC_UBOOT_SPL_NAME $TARGET_UBOOT_DIR/$TARGET_UBOOT_SPL_NAME
-fi
+function clean()
+{
+    make clean
+}
+
+# include
+. $INCLUDE_FUNCS
+# include end
+
+cd $(dirname $0)
+
+buildopt $1
