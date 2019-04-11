6b42c59a537b79296995ae8a62aa576245081c0e
yunqian.wang <yunqian.wang@horizon.ai>
2019-04-11 17:33:01 +0800

    mtd: fix som board nor flash bug

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -68,7 +68,7 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"gd25q32b",       INFO(0xc84016, 0x0, 64 * 1024,    64, SECT_4K) },
 	{"gd25lq32",	   INFO(0xc86016, 0x0, 64 * 1024,    64, SECT_4K) },
 	{"gd25lq256d",     INFO(0xc86019, 0x0, 64 * 1024,   512, RD_FULL | WR_QPP | SECT_4K) },
-	{"gd25lq128d",     INFO(0xc86018, 0x0, 64 * 1024,   256, RD_FULL | WR_QPP | SECT_4K) },
+	{"gd25lq128d",     INFO(0xc86018, 0x0, 64 * 1024,   256, SECT_4K) },
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	{"is25lq040b",	   INFO(0x9d4013, 0x0, 64 * 1024,    8, 0)  },
