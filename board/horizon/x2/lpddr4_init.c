d8c8e17ee43bf9850d4b3f9af6acc067a113c730
Yunqian Wang <yunqian.wang@horizon.ai>
2019-10-15 14:07:22 +0800

    spl: fix wakeup_staus reg of pmu is not incorrect
    Summary:
    	the value of wakeup_staus reg should be 0 after system reboot.
    	if x2/j2 soc awaked by sync or async source then execute
    	reboot in linux kernel,the value of wakeup_staus reg is not power on
    	reset.
    
    Test Plan: no plan
    
    Reviewers: huiliang.xu, xiaofeng.ling
    
    Reviewed By: huiliang.xu, xiaofeng.ling
    
    Differential Revision: https://cr.hobot.cc/D10396

--- board/horizon/x2/lpddr4_init.c
+++ board/horizon/x2/lpddr4_init.c
@@ -683,6 +683,7 @@ void ddr_init(struct dram_timing_info *dram_timing)
 	unsigned int txdqsdly_coarse, txdqsdly_fine, trained_txdqsdly;
 	unsigned int tctrl_delay, t_wrdata_delay;
 	unsigned int wk_sta;
+	unsigned int resume_addr;
 
 	dram_pll_init(MHZ(g_ddr_rate));
 
@@ -726,7 +727,9 @@ void ddr_init(struct dram_timing_info *dram_timing)
 	wk_sta = readl(X2_PMU_WAKEUP_STA);
 	printf("wake sta = 0x%x, src = 0x%x\n", wk_sta, readl(X2_PMU_W_SRC));
 
-	if (wk_sta == 3)
+	resume_addr = readl(X2_PMU_SW_REG_03);
+
+	if (wk_sta != 0 && resume_addr == 0)
 		wk_sta = 0;
 
 	lpddr4_cfg_fw(dram_timing, wk_sta);
@@ -848,8 +851,9 @@ void ddr_init(struct dram_timing_info *dram_timing)
 #endif
 	} else if (wk_sta == 2 || wk_sta == 1) {
 		void (*x2_resume)(void);
-		unsigned long p = readl(X2_PMU_SW_REG_03);
-		x2_resume = (void(*)(void))p;
+		x2_resume = (void(*)(void))resume_addr;
+
+		writel(0, X2_PMU_SW_REG_03);
 
 		invalidate_icache_all();
 		x2_resume();
