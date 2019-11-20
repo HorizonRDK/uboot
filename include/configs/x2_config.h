b28bbffe50ca57c8fa75812db4b1c5275d7af1a8
dinggao.pan <dinggao.pan@horizon.ai>
2019-11-20 22:20:04 +0800

    bugfix: fixed nor boot failure
    Summary:
    fixed nor boot failure caused by added ubi fastmap
    configuration.
    
    Test Plan:
    Build nor image successfully and boot from nor flash
    successfully
    
    Reviewers: huiliang.xu, xiaofeng.ling
    
    Reviewed By: huiliang.xu, xiaofeng.ling
    
    Differential Revision: https://cr.hobot.cc/D18042

--- include/configs/x2_config.h
+++ include/configs/x2_config.h
@@ -1,10 +1,16 @@
+/*
+ * Hobot X2 Board Configuration header
+ *
+ * Copyright (C) 2019 Horizon, Inc.
+ * All rights reserved.
+ */
 #ifndef __X2_CONFIG_H__
 #define __X2_CONFIG_H__
 /* #define CONFIG_X2_AP_BOOT */
 /* #define CONFIG_X2_YMODEM_BOOT */
-#define CONFIG_X2_NAND_BOOT
+/* #define CONFIG_X2_NAND_BOOT */
 /* #define CONFIG_X2_NOR_BOOT */
-/* #define CONFIG_X2_MMC_BOOT */
+#define CONFIG_X2_MMC_BOOT
 
 #define CONFIG_X2_LPDDR4_2666 (2666)
 
