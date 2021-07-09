HorizonRDK/uboot

commit fd9dbe85606f56afb8885776a8487a5d7a3799a8

spi: hb_qspi: bug [XJ3-3286] fix line mode configuration fail
Summary:
fix line mode configuration fail due to incorrect access to the driver
plat data.

Test Plan: Configure line mode through dts okay

Reviewers: ming.yu, yunqian.wang

Reviewed By: ming.yu

Differential Revision: https://cr.hobot.cc/D94721

drivers/spi/hb_qspi.c
@@ -704,7 +704,7 @@ exec_end:
 static bool hb_supports_op(struct spi_slave *slave,
 						   const struct spi_mem_op *op)
 {
-	struct hb_qspi_platdata *slave_plat = dev_get_parent_platdata(slave->dev);
+	struct hb_qspi_platdata *slave_plat = plat;
 	if (op->cmd.buswidth > 1
 		|| op->addr.buswidth > slave_plat->bus_width
 		|| op->dummy.buswidth > slave_plat->bus_width

commit 8c1d28a8e8ed74acad26472b466c1aa5a740700f

spi: hb_qspi: bug [XJ3-3376] fix nor read fail due to dts buswidth get fail
Summary:
fixed a bug introduced by commit fd9dbe85606f56afb8885776a8487a5d7a3799a8 where
the buswidth is retrived from the wrong address.

Test Plan: nor boot okay

Reviewers: ming.yu

Reviewed By: ming.yu

Differential Revision: https://cr.hobot.cc/D98898

arch/arm/dts/hobot-xj3-soc.dts
@@ -56,21 +56,19 @@
 	#address-cells = <1>;
 	#size-cells = <0>;
 	spi-max-frequency = <62500000>;
+	spi-tx-bus-width = <4>;
+	spi-rx-bus-width = <4>;
 
 	is25wp512@0 {
 		u-boot,dm-pre-reloc;
 		compatible = "jedec,spi-nor";
 		reg = <0x0>;
-		spi-tx-bus-width = <4>;
-		spi-rx-bus-width = <4>;
 	};
 
 	5f1gq4rc9ig@0 {
 		u-boot,dm-pre-reloc;
 		compatible = "spi-nand";
 		reg = <0x0>;
-		spi-tx-bus-width = <4>;
-		spi-rx-bus-width = <4>;
 	};
 
 };

drivers/spi/hb_qspi.c
@@ -747,6 +747,8 @@ static int hb_qspi_ofdata_to_platdata(struct udevice *bus)
 		plat->hclk = fdtdec_get_int(blob, node,
 									"spi-max-frequency", HB_QSPI_DEF_BR);
 	}
+	debug("%s:%d plat: sclk:%d hclk:%d, buswidth:%d\n", __func__, __LINE__,
+				plat->sclk, plat->hclk, plat->bus_width);
 	// TODO: Chip Select Define
 	return 0;
 }