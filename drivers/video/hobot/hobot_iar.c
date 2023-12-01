b8bc388d798828cf668fd5ba03c1e5aff8da34ae
luojiale <jiale.luo@horizon.ai>
2023-12-01 21:10:04 +0800

    fix: disable usbkbd features
    Signed-off-by: luojiale <jiale.luo@horizon.ai>

--- drivers/video/hobot/hobot_iar.c
+++ drivers/video/hobot/hobot_iar.c
@@ -327,7 +327,7 @@ static void xj3_bt1120_pinmux_switch(void)
 
         // write back
         writel(reg_value, bt1120_addr);
-        // xj3_display_debug("BT1120 [reg]: %x,[val]: %x\n", bt1120_addr, reg_value);
+        printf("BT1120 [reg]: %x,[val]: %x\n", bt1120_addr, reg_value);
     }
 }
 
